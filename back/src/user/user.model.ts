d9996e16a5e2f01faceb1e428ceb3999cbf64894
nikitenkovit <nelot@mail.ru>
2024-08-27 20:01:07 +0300

    Добавит токены

--- back/src/user/user.model.ts
+++ back/src/user/user.model.ts
@@ -1,8 +1,10 @@
 import { prop } from '@typegoose/typegoose';
 import { Base, TimeStamps } from '@typegoose/typegoose/lib/defaultClasses';
 
+// eslint-disable-next-line @typescript-eslint/no-unsafe-declaration-merging
 export interface UserModel extends Base {}
 
+// eslint-disable-next-line @typescript-eslint/no-unsafe-declaration-merging
 export class UserModel extends TimeStamps {
   @prop({ unique: true })
   email: string;
