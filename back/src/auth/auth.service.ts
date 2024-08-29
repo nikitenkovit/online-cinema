125a8bc4cda475f774d7b107d030cf763cfa3c02
nikitenkovit <nelot@mail.ru>
2024-08-29 22:09:33 +0300

    Добавит возможность обновлять, удалять и получать пользователей

--- back/src/auth/auth.service.ts
+++ back/src/auth/auth.service.ts
@@ -1,7 +1,7 @@
 import {
-  BadRequestException,
-  Injectable,
-  UnauthorizedException,
+	BadRequestException,
+	Injectable,
+	UnauthorizedException,
 } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { ModelType } from '@typegoose/typegoose/lib/types';
@@ -13,100 +13,102 @@ import { RefreshTokenDto } from './dto/refreshToken.dto';
 
 @Injectable()
 export class AuthService {
-  constructor(
-    @InjectModel(UserModel) private readonly UserModel: ModelType<UserModel>,
-    private readonly jwtService: JwtService
-  ) {}
+	constructor(
+		@InjectModel(UserModel) private readonly UserModel: ModelType<UserModel>,
+		private readonly jwtService: JwtService
+	) {}
 
-  async login(dto: AuthDto) {
-    const user = await this.validateUser(dto);
+	async login(dto: AuthDto) {
+		const user = await this.validateUser(dto);
 
-    const tokens = await this.issueTokenPair(String(user._id));
+		const tokens = await this.issueTokenPair(String(user._id));
 
-    return {
-      user: this.returnUserFields(user),
-      ...tokens,
-    };
-  }
+		return {
+			user: this.returnUserFields(user),
+			...tokens,
+		};
+	}
 
-  async getNewTokens({ refreshToken }: RefreshTokenDto) {
-    if (!refreshToken) {
-      throw new UnauthorizedException('Please sign in');
-    }
+	async getNewTokens({ refreshToken }: RefreshTokenDto) {
+		if (!refreshToken) {
+			throw new UnauthorizedException('Please sign in');
+		}
 
-    const result = await this.jwtService.verifyAsync(refreshToken);
+		const result = await this.jwtService.verifyAsync(refreshToken);
 
-    if (!result) {
-      throw new UnauthorizedException('Invalid token or expired!');
-    }
+		if (!result) {
+			throw new UnauthorizedException('Invalid token or expired!');
+		}
 
-    const user = await this.UserModel.findById(result._id);
+		const user = await this.UserModel.findById(result._id);
 
-    const tokens = await this.issueTokenPair(String(user._id));
+		const tokens = await this.issueTokenPair(String(user._id));
 
-    return {
-      user: this.returnUserFields(user),
-      ...tokens,
-    };
-  }
+		return {
+			user: this.returnUserFields(user),
+			...tokens,
+		};
+	}
 
-  async register(dto: AuthDto) {
-    const oldUser = await this.UserModel.findOne({ email: dto.email });
+	async register(dto: AuthDto) {
+		const oldUser = await this.UserModel.findOne({ email: dto.email });
 
-    if (oldUser) {
-      throw new BadRequestException('User already exists');
-    }
+		if (oldUser) {
+			throw new BadRequestException('User already exists');
+		}
 
-    const salt = await genSalt(10);
+		const salt = await genSalt(10);
 
-    const newUser = new this.UserModel({
-      email: dto.email,
-      password: await hash(dto.password, salt),
-    });
+		const newUser = new this.UserModel({
+			email: dto.email,
+			password: await hash(dto.password, salt),
+		});
 
-    const tokens = await this.issueTokenPair(String(newUser._id));
+		const user = await newUser.save();
 
-    return {
-      user: this.returnUserFields(newUser),
-      ...tokens,
-    };
-  }
+		const tokens = await this.issueTokenPair(String(user._id));
 
-  async validateUser(dto: AuthDto): Promise<UserModel> {
-    const user = await this.UserModel.findOne({ email: dto.email });
+		return {
+			user: this.returnUserFields(user),
+			...tokens,
+		};
+	}
 
-    if (!user) {
-      throw new UnauthorizedException('User not found');
-    }
+	async validateUser(dto: AuthDto): Promise<UserModel> {
+		const user = await this.UserModel.findOne({ email: dto.email });
 
-    const isValid = await compare(dto.password, user.password);
+		if (!user) {
+			throw new UnauthorizedException('User not found');
+		}
 
-    if (!isValid) {
-      throw new UnauthorizedException('Invalid password');
-    }
+		const isValid = await compare(dto.password, user.password);
 
-    return user;
-  }
+		if (!isValid) {
+			throw new UnauthorizedException('Invalid password');
+		}
 
-  async issueTokenPair(userId: string) {
-    const data = { _id: userId };
+		return user;
+	}
 
-    const refreshToken = await this.jwtService.signAsync(data, {
-      expiresIn: '15d',
-    });
+	async issueTokenPair(userId: string) {
+		const data = { _id: userId };
 
-    const accessToken = await this.jwtService.signAsync(data, {
-      expiresIn: '1h',
-    });
+		const refreshToken = await this.jwtService.signAsync(data, {
+			expiresIn: '15d',
+		});
 
-    return { refreshToken, accessToken };
-  }
+		const accessToken = await this.jwtService.signAsync(data, {
+			expiresIn: '1h',
+		});
 
-  returnUserFields(user: UserModel) {
-    return {
-      _id: user._id,
-      email: user.email,
-      isAdmin: user.isAdmin,
-    };
-  }
+		return { refreshToken, accessToken };
+	}
+
+	returnUserFields(user: UserModel) {
+		return {
+			_id: user._id,
+			email: user.email,
+			isAdmin: user.isAdmin,
+		};
+	}
 }
