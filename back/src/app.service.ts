8e010b69d524f35d6793527af43fb5da1b46a4a9
nikitenkovit <nelot@mail.ru>
2024-09-04 16:38:31 +0300

    Добавит загрузку файлов

--- back/src/app.service.ts
+++ back/src/app.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello() {
-    return { text: 'Hello World!' };
-  }
+	getHello() {
+		return { text: 'Hello World!' };
+	}
 }
